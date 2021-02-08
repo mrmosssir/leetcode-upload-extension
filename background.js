a08c5f44b9a60f2a99e2a1ed7cbba79b5ea515d7
SuTo <mrmosssir@gmail.com>
2021-02-08 22:28:48 +0800

    version 0.1 alpha complete

--- background.js
+++ background.js
@@ -1,114 +1,31 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-//   if (message.methods === "send-backup") {
-//     let data = message.data;
-//     let title = data.title;
-//     let code = "";
-//     data.code.forEach((item) => {
-//       code += `${item}\n`;
-//     });
-//     console.log(`title : ${title}\ncode: ${code}`);
-//     // upload_google_drive();
-//   }
-//   if (message.methods === "send-login") {
-//     handleAuthClick();
-//     // console.log(message.methods);
-//   }
-  if (message.methods === "test") {
-    handleClientLoad();
+  if (message.methods === "send-backup") {
+    let data = message.data;
+    let title = data.title;
+    let code = "";
+    data.code.forEach((item) => {
+      code += `${item}\n`;
+    });
+    chrome.identity.getAuthToken({interactive: true}, (token) => {
+      console.log('got the token', token);
+      let metadata = {
+        name: `${title}.js`,
+        mimeType: 'text/javascript',
+        parents: ['1cNG5q7Wzv_deLgHFUcHkWcq-LV9P2k-p'],
+      };
+      let file = new Blob([code], {type: 'text/javascript'});
+      var form = new FormData();
+      form.append('metadata', new Blob([JSON.stringify(metadata)], {type: 'application/json'}));
+      form.append('file', file);
+
+      var xhr = new XMLHttpRequest();
+      xhr.open('POST', 'https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart');
+      xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+      xhr.responseType = 'json';
+      xhr.onload = () => {
+          console.log(xhr.response);
+      };
+      xhr.send(form);
+    })
   }
 });
-
-var GoogleAuth;
-
-let head = document.getElementsByTagName("head")[0];
-let script = document.createElement("script");
-script.type = "text/javascript";
-script.src = "https://apis.google.com/js/api.js";
-head.appendChild(script);
-
-
-function handleClientLoad() {
-  gapi.load("client:auth2", initClient);
-}
-
-function initClient() {
-  let client_id =
-    "451555466829-5ktrvo3sfa9to64ofhr7leca2ja49kno.apps.googleusercontent.com";
-  let api_key = "AIzaSyDA4YJLziXW_Qrd8j544vQv_tvci1LxnAo";
-  let scope = "https://www.googleapis.com/auth/drive.metadata.readonly";
-  gapi.client.init({
-      'apiKey': api_key,
-      'clientId': client_id,
-      'scope': scope,
-      'discoveryDocs': [
-        "https://www.googleapis.com/discovery/v1/apis/drive/v3/rest",
-      ],
-    })
-    .then(() => {
-        // Listen for sign-in state changes.
-        GoogleAuth = gapi.auth2.getAuthInstance();
-        // console.log(GoogleAuth);
-        GoogleAuth.isSignedIn.listen(updateSigninStatus);
-        let user = GoogleAuth.currentUser.get();
-        setSigninStatus();
-
-        chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-            // if (message.methods === "send-backup") {
-            //   let data = message.data;
-            //   let title = data.title;
-            //   let code = "";
-            //   data.code.forEach((item) => {
-            //     code += `${item}\n`;
-            //   });
-            //   console.log(`title : ${title}\ncode: ${code}`);
-            //   // upload_google_drive();
-            // }
-            console.log(message);
-            if (message.methods === "send-login") {
-              handleAuthClick();
-              // console.log(message.methods);
-            }
-            // if (message.methods === "test") {
-            //   handleClientLoad();
-            // }
-          });
-
-        // Handle the initial sign-in state.
-        // updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-        // authorizeButton.onclick = handleAuthClick;
-        // signoutButton.onclick = handleSignoutClick;
-      },
-      function (error) {
-        console.log(error);
-      }
-    );
-}
-
-function handleAuthClick() {
-  GoogleAuth = gapi.auth2.getAuthInstance();
-  if (GoogleAuth.isSignedIn.get()) {
-    // User is authorized and has clicked "Sign out" button.
-    GoogleAuth.signOut();
-  } else {
-    // User is not signed in. Start Google auth flow.
-    GoogleAuth.signIn();
-  }
-}
-
-function setSigninStatus() {
-  var user = GoogleAuth.currentUser.get();
-  var isAuthorized = user.hasGrantedScopes(SCOPE);
-  if (isAuthorized) {
-    chrome.runtime.sendMessage({
-      methods: "check-login",
-    });
-  } else {
-    chrome.runtime.sendMessage({
-      methods: "check-logout",
-    });
-  }
-}
-
-function updateSigninStatus() {
-  setSigninStatus();
-}
--- option.js
+++ option.js
@@ -1,48 +1,11 @@
-var GoogleAuth;
-
-var client_id =
-  "451555466829-91vgcfitg3f5h70sv7h1547vi980a8n2.apps.googleusercontent.com";
-var api_key = "AIzaSyDA4YJLziXW_Qrd8j544vQv_tvci1LxnAo";
-var scope = "https://www.googleapis.com/auth/drive.metadata.readonly";
-
 let btn = document.querySelector("#btn");
-let loginBtn = document.querySelector("#btn-login");
 
-let head = document.getElementsByTagName("head")[0];
-let script = document.createElement("script");
-script.type = "text/javascript";
-script.src = "https://apis.google.com/js/api.js";
-head.appendChild(script);
 
 btn.addEventListener("click", (e) => {
   chrome.tabs.executeScript(null, { file: "content.js" }, () => {
     connect();
   });
 });
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  switch (message.methods) {
-    case "send-backup":
-      let data = message.data;
-      let title = data.title;
-      let code = "";
-      data.code.forEach((item) => {
-        code += `${item}\n`;
-      });
-      alert(`title : ${title}\ncode: ${code}`);
-      break;
-    // case "check-login":
-    //   loginBtn.textContent = "logout";
-    //   break;
-    // case "check-logout":
-    //   loginBtn.textContent = "login";
-    //   break;
-  }
-});
-
-window.onload = () => {
-  handleClientLoad();
-};
 
 function connect() {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
@@ -52,88 +15,4 @@ function connect() {
       alert(response.title);
     });
   });
-}
-
-function handleClientLoad() {
-  gapi.load("client:auth2", initClient);
-}
-
-function initClient() {
-  alert("enter");
-  gapi.client
-    .init({
-      apiKey: api_key,
-      clientId: client_id,
-      scope: scope,
-      discoveryDocs: [
-        "https://www.googleapis.com/discovery/v1/apis/drive/v3/rest",
-      ],
-    })
-    .then(
-      () => {
-        // Listen for sign-in state changes.
-        GoogleAuth = gapi.auth2.getAuthInstance();
-        // console.log(GoogleAuth);
-        GoogleAuth.isSignedIn.listen(updateSigninStatus);
-        let user = GoogleAuth.currentUser.get();
-        setSigninStatus();
-
-        // if (message.methods === "send-backup") {
-        //   let data = message.data;
-        //   let title = data.title;
-        //   let code = "";
-        //   data.code.forEach((item) => {
-        //     code += `${item}\n`;
-        //   });
-        //   console.log(`title : ${title}\ncode: ${code}`);
-        //   // upload_google_drive();
-        // }
-        loginBtn.addEventListener("click", (e) => {
-          handleAuthClick();
-        });
-
-        // console.log(message.methods);
-
-        // if (message.methods === "test") {
-        //   handleClientLoad();
-        // }
-
-        // Handle the initial sign-in state.
-        // updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-        // authorizeButton.onclick = handleAuthClick;
-        // signoutButton.onclick = handleSignoutClick;
-      },
-      function (error) {
-        alert(error);
-      }
-    );
-}
-
-function handleAuthClick() {
-  alert("done");
-  GoogleAuth = gapi.auth2.getAuthInstance();
-  if (GoogleAuth.isSignedIn.get()) {
-    // User is authorized and has clicked "Sign out" button.
-    GoogleAuth.signOut();
-  } else {
-    // User is not signed in. Start Google auth flow.
-    GoogleAuth.signIn();
-  }
-}
-
-function setSigninStatus() {
-  var user = GoogleAuth.currentUser.get();
-  var isAuthorized = user.hasGrantedScopes(scope);
-  alert(isAuthorized);
-  if (isAuthorized) {
-    // loginBtn.textContent = "logout";
-    alert('logout');
-  } else {
-    // loginBtn.textContent = "login";
-    alert('login');
-  }
-}
-
-function updateSigninStatus() {
-  setSigninStatus();
 }
